58f53d70df8020efd3f56ca5ebc5d8c7f6320e9f
Nick Francisci <nickfrancisci@gmail.com>
2022-01-08 00:06:00 -0500

    Finish user sign-out, cancel account redirects

--- app/controllers/splash_controller.rb
+++
@@ -1,4 +0,0 @@
-class SplashController < ActionController::Base
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  # Workaround because something about Rails 7 (Turbo) doesn't seem to play
+  # well with Devise's after_sign_out_path_for; it completely ignores it.
+  # Instead, it always redirects to users_url, so we just point that back
+  # to the home. However, that prevents us from having a user index page,
+  # which we may well want!
+  get "users", to: "home#index"
 
-  # Defines the root path route ("/")
-  root to: "splash#index"
-  get "/splash", to: "splash#index"
   get "/dashboard", to: "dashboard#index"
-  # resources "users"
   devise_for :users
+  devise_for :admins
 end
